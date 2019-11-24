6adb4a404bf4f7e8da71c8714ddf90b2afac196e
Américo <albocchini@gmail.com>
2019-11-24 11:31:02 -0300

    Update user and Validations

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -4,17 +4,16 @@ import { promisify } from 'util';
 require('dotenv/config');
 
 export default async (req, res, next) => {
-  const authHeader = req.headears.authorization;
+  const authHeader = req.headers.authorization;
 
   if (!authHeader) {
     return res.status(401).json({ error: 'Token not provider' });
   }
 
-  const [, token] = authHeader.split('');
+  const [, token] = authHeader.split(' ');
 
   try {
     const decoded = await promisify(jwt.verify)(token, process.env.APP_SECRET);
-
     req.userId = decoded.id;
     return next();
   } catch (err) {
